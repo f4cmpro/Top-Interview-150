96cade6c9f0c4ff9798638355d9ea265f2ffbe57
tu.doan <tu.doan@lge.com>
2026-04-21 17:13:03 +0700

    In progress: # 138. Copy List with Random Pointer

--- problems/linked-list/medium/138-copy-list-with-random-pointer/Solution.kt
+++ problems/linked-list/medium/138-copy-list-with-random-pointer/Solution.kt
@@ -1,3 +1,19 @@
 class Solution {
-    
+    fun copyRandomList(node: Node?): Node? {
+        if (node == null) return null
+        var current = node
+        val copyNode : Node? = null
+        var copyCurrent = copyNode
+        while (current != null) {
+            val newNode = Node(current.`val`)
+            newNode.next = if (current.next != null) Node(node.next!!.`val`) else null
+            newNode.random = if (current.random != null) Node(current.random!!.`val`) else null
+            copyCurrent = newNode
+        }
+    }
+}
+
+class Node(var `val`: Int) {
+    var next: Node? = null
+    var random: Node? = null
 }
