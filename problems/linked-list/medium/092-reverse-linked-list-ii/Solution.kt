226e3152518c054e49e7bffe059cc6b859bd9e1c
tu.doan <tu.doan@lge.com>
2026-04-22 17:08:06 +0700

    In progress:# 92 - Reverse Linked List II

--- problems/linked-list/medium/092-reverse-linked-list-ii/Solution.kt
+++ problems/linked-list/medium/092-reverse-linked-list-ii/Solution.kt
@@ -1,3 +1,32 @@
 class Solution {
-    
+    fun reverseBetween(head: ListNode?, left: Int, right: Int): ListNode? {
+        if (left == right) return head
+        val listNode = ArrayList<ListNode>()
+        var current = head
+        var start = left - 1
+        var end = right - 1
+        while (current != null) {
+            listNode.add(current)
+            current = current.next
+        }
+        while (start < end) {
+            val temp = listNode[start]
+            listNode[start] = listNode[end]
+            listNode[end] = temp
+            start++
+            end--
+        }
+        val dummyHead = ListNode(0)
+        current = dummyHead
+        for (node in listNode) {
+            current?.next = node
+            current = current?.next
+        }
+        current?.next = null
+        return dummyHead.next
+    }
+}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
 }
