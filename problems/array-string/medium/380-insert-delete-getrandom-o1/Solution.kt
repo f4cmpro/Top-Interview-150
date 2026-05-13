b7f3347e1a99763690d0e7aef266ffdcbe684f19
tu.doan <tu.doan@lge.com>
2026-05-13 15:34:52 +0700

    Inprogress: # 380 - Insert Delete GetRandom O(1)

--- problems/array-string/medium/380-insert-delete-getrandom-o1/Solution.kt
+++ problems/array-string/medium/380-insert-delete-getrandom-o1/Solution.kt
@@ -1,3 +1,33 @@
-class Solution {
-    
+import kotlin.random.Random
+
+class RandomizedSet() {
+    val set = HashSet<Int>()
+    var theLast = 0
+    fun insert(`val`: Int): Boolean {
+        if (set.contains(`val`)) {
+            return false
+        }
+        set.add(`val`)
+        return true
+    }
+
+    fun remove(`val`: Int): Boolean {
+        if (!set.contains(`val`)) {
+            return false
+        }
+        set.remove(`val`)
+        if (set.isEmpty()) {
+            theLast = `val`
+        }
+        return true
+    }
+
+    fun getRandom(): Int {
+        return if (set.isEmpty()) {
+            theLast
+        } else {
+            set.random()
+        }
+    }
+
 }
