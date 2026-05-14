e2e479c3d6ad93b148a247911f51a9832c764b46
tu.doan <tu.doan@lge.com>
2026-05-14 17:06:07 +0700

    Inprogress: # 134 - Gas Station

--- problems/array-string/medium/134-gas-station/Solution.kt
+++ problems/array-string/medium/134-gas-station/Solution.kt
@@ -1,3 +1,24 @@
 class Solution {
-    
+    fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
+        var startIndex = -1
+        for (i in gas.indices) {
+            if (gas[i] - cost[i] >= 0) {
+                startIndex = i
+                break
+            }
+        }
+        if (startIndex == -1) {
+            return -1
+        }
+        var restTank = gas[startIndex] - cost[startIndex]
+        var nextIndex = startIndex + 1
+        while (nextIndex != startIndex) {
+            restTank += gas[nextIndex] - cost[nextIndex]
+            if (restTank < 0) {
+                return -1
+            }
+            nextIndex =
+        }
+        return startIndex
+    }
 }
